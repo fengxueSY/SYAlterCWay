a81fab7c39665a39baad2a011ca1fe66b29d021d
fengxueSY <xyz123003@126.com>
2017-05-08 13:51:56 +0800

    add

---
+++ BPCWay/BPCWay.h
@@ -0,0 +1,22 @@
+//
+//  BPCWay.h
+//  BasePlatform
+//
+//  Created by 666GPS on 2016/12/26.
+//  Copyright © 2016年 yang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+/**
+ *  c语言定义提示框
+ *
+ *  @param message 要显示的信息， view 要显示的view，一般填写self
+ *  @param view    nil
+ */
+void TTAlert1(NSString * message,UIViewController * view);
+/**
+ *  在view上面显示提示框
+ *
+ *  @param message 显示提示框的内容
+ */
+void TTAlert2(NSString * message);
